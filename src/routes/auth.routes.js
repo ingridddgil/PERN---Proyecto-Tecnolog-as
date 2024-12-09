84affdecda1dbbba0217bcba004c5ed9abf5a0d3
ingridd <ingrid.sglh@gmail.com>
2024-12-08 23:42:38 -0600

    ahi va

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import {Router} from 'express'
+
+const router = Router();
+
+router.post('/signin', (req, res) => res.send("Signing in..."));
+router.post('/signup', (req, res) => res.send("Signing up..."));
+router.post('/logout', (req, res) => res.send("Logging out..."));
+router.get('/profile', (req, res) => res.send("User profile..."));
+
+
+export default router;
---
+++ src/routes/task.routes.js
@@ -0,0 +1,11 @@
+import {Router} from 'express'
+
+const router = Router();
+
+router.get('/tasks', (req, res) => res.send('Getting tasks...'))
+router.get('/tasks/:id', (req, res) => res.send('Getting task...'))
+router.post('/tasks', (req, res) => res.send('Creating task...'))
+router.put('/tasks/:id', (req, res) => res.send('Updating task...'))
+router.delete('/tasks/:id', (req, res) => res.send('Deleting task...'))
+
+export default router;
